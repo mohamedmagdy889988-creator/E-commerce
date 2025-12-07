86c00e210e84fcd5ba5fab2f3228a0c73ef57d70
Mohamed Magdy <mohamedmagdy889988@gmail.com>
2025-12-07 14:50:29 +0200

    change context

--- src/context/cart.context.tsx
+++ src/context/cart.context.tsx
@@ -1,3 +1,31 @@
+// "use client";
+
+// import { createContext, ReactNode, useState } from "react";
+
+// interface ICartContext {
+//     cartCount: number;
+//     updateCartCount: (x: number) => void;
+// }
+
+// export const CartContext = createContext<ICartContext>({
+//     cartCount: 0,
+//     updateCartCount: () => {}
+// });
+
+// export function CartCountProvider({ children }: { children: ReactNode }) {
+//     const [cartCount, setCartCount] = useState(0);
+
+//     function updateCartCount(newCount: number) {
+//         setCartCount(newCount);
+//     }
+
+//     return (
+//         <CartContext.Provider value={{ cartCount, updateCartCount }}>
+//             {children}
+//         </CartContext.Provider>
+//     );
+// }
+
 "use client";
 
 import { createContext, ReactNode, useState } from "react";
@@ -5,11 +33,13 @@ import { createContext, ReactNode, useState } from "react";
 interface ICartContext {
     cartCount: number;
     updateCartCount: (x: number) => void;
+    resetCartCount: () => void; 
 }
 
 export const CartContext = createContext<ICartContext>({
     cartCount: 0,
-    updateCartCount: () => {}
+    updateCartCount: () => {},
+    resetCartCount: () => {},
 });
 
 export function CartCountProvider({ children }: { children: ReactNode }) {
@@ -19,8 +49,12 @@ export function CartCountProvider({ children }: { children: ReactNode }) {
         setCartCount(newCount);
     }
 
+    function resetCartCount() {
+        setCartCount(0);
+    }
+
     return (
-        <CartContext.Provider value={{ cartCount, updateCartCount }}>
+        <CartContext.Provider value={{ cartCount, updateCartCount, resetCartCount }}>
             {children}
         </CartContext.Provider>
     );
